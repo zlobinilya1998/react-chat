7ee63d89b1cd42148a7151e9f6c91c03b228093d
zlobin_ilya <qwertin1998@gmail.com>
2021-02-12 13:11:58 +0300

    send msg

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import "./style/App.scss";
 
 let ws = new WebSocket(`wss://react-chat-roky.herokuapp.com/`);
 
-ws.onopen = () => {
-  alert(`Соединение установленно`);
+ws.m;
+
+ws.onopen = function (e) {
+  alert(`open`);
+  ws.send(`Меня зовут джон`);
 };
 
 function App() {
